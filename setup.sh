ea99541996351b95ff0f41d6e2d532ba42005e44
lisi-123 <160303845+lisi-123@users.noreply.github.com>
2024-12-19 23:46:28 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 更新软件包列表
+apt-get update
+
+# 安装必需的软件包
+apt install sudo -y
+sudo apt install curl -y
+sudo apt-get install ufw -y
+sudo apt install nano -y
+
+# 安装xrayr
+wget -N https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh && bash install.sh v0.9.4
+
+# 替换配置文件
+wget -N https://raw.githubusercontent.com/lisi-123/xrayr-warp/main/config.yml -P /etc/XrayR/
+wget -N https://raw.githubusercontent.com/lisi-123/xrayr-warp/main/custom_outbound.json -P /etc/XrayR/
+wget -N https://raw.githubusercontent.com/lisi-123/xrayr-warp/main/dns.json -P /etc/XrayR/
+wget -N https://raw.githubusercontent.com/lisi-123/xrayr-warp/main/route.json -P /etc/XrayR/
+
+# 安装warp并搭建socks代理
+wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh && bash menu.sh <<< $'2\n13\n40000\n1\n'
+
+echo "已自动配置warp解锁，在config.yml搭建节点后生效"
