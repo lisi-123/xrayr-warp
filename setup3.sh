c0d14bcf9a9ec4cdf3666e01f33eaa62436c66e4
lisi-123 <160303845+lisi-123@users.noreply.github.com>
2024-12-20 15:31:48 +0800

    Update setup3.sh

--- setup3.sh
+++ setup3.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# 确保脚本以 root 用户运行
+if [ "$(id -u)" -ne 0 ]; then
+  echo "请以 root 用户或使用 sudo 运行此脚本"
+  exit 1
+fi
+
 # 更新软件包列表
 apt-get update
 
 # 安装必需的软件包
-apt install sudo -y
-sudo apt install curl -y
-sudo apt-get install ufw -y
-sudo apt install nano -y
+apt-get install sudo curl ufw nano -y
 
-# 安装xrayr
+# 安装 xrayr
 wget -N https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh && bash install.sh v0.9.4
 
 # 替换配置文件
@@ -18,7 +21,7 @@ wget -N https://raw.githubusercontent.com/lisi-123/xrayr-warp/main/custom_outbou
 wget -N https://raw.githubusercontent.com/lisi-123/xrayr-warp/main/dns.json -P /etc/XrayR/
 wget -N https://raw.githubusercontent.com/lisi-123/xrayr-warp/main/route.json -P /etc/XrayR/
 
-# 安装warp并搭建socks代理
+# 安装 warp 并搭建 socks 代理
 wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh && bash menu.sh <<< $'2\n13\n40000\n1\n'
 
-echo "已自动配置warp解锁，在config.yml搭建节点后生效"
+echo "已自动配置 warp 解锁，在 config.yml 搭建节点后生效"
